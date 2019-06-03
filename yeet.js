5683da4c1a3f755a8e63bdbc72c1a5dd8ef99dda
Lukas Horak <lukasbhorak@gmail.com>
2019-06-03 14:19:38 -0400

    added yeet back in, had opportunity to throw

---
+++ yeet.js
@@ -0,0 +1,7 @@
+// Cause why throw an error when you can YEET(https://www.youtube.com/watch?v=2Bjy5YQ5xPc) it
+module.exports = {
+  YEET: (err) => {
+    console.log("This code errored. YEET \n*DABS*\n\n \\(⌐■_■)ᕗ\n---")
+    throw err
+  }
+}
