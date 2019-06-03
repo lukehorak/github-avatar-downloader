1a2dae8272768b895d8f78c5d9ea0b2789823148
Lukas Horak <lukasbhorak@gmail.com>
2019-06-03 14:20:05 -0400

    logging avi urls

--- download_avitars.js
+++ download_avitars.js
@@ -1,10 +1,11 @@
 const request = require('request');
 const secrets = require('./secrets');
+const YEET = require('./yeet');
 
 
 const getRepoContributors = (repoOwner, repoName, cb) => {
   const options = {
-    url: `https://api.github.com/repos/${repoOwner}/${repoName}`,
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
     headers :{
       'User-Agent': 'request',
       'authorization': `${secrets.GITHUB_USER}:${secrets.GITHUB_TOKEN}`
@@ -12,13 +13,18 @@ const getRepoContributors = (repoOwner, repoName, cb) => {
   };
 
   request(options, function(err, res, body){
-    cb(err, body);
+    const jsonResults = JSON.parse(body);
+    let resultURLs = [];
+    for (user of jsonResults){
+      resultURLs.push(user.avatar_url)
+    }
+    cb(err, resultURLs);
   });
 }
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
+  if (err) YEET(err);
   console.log("Result:", result)
 })
