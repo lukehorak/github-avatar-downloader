92c245c084ecb5ed70d4b6ef79e0bc65968b7ce1
Lukas Horak <lukasbhorak@gmail.com>
2019-06-03 16:03:11 -0400

    added required command line args

--- download_avitars.js
+++ download_avitars.js
@@ -5,39 +5,49 @@ const fs = require('fs');
 // Function to download avatars, given url
 const downloadImageByURL = (url, filePath) => {
   request.get(url)
-  .on('error', function(err){
-    throw err;
-  })
-  .pipe(fs.createWriteStream(filePath))
+    .on('error', function (err) {
+      throw err;
+    })
+    .pipe(fs.createWriteStream(filePath))
 }
 
 // Get all contributors to repo, peform callback on them
 const getRepoContributors = (repoOwner, repoName, cb) => {
   const options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
-    headers :{
+    headers: {
       'User-Agent': 'request',
       'authorization': `${secrets.GITHUB_USER}:${secrets.GITHUB_TOKEN}`
     }
   };
 
-  request(options, function(err, res, body){
-    cb(err, body);
-  });
+  request(options, function (err, res, body) {
+      cb(err, body);
+    })
+    .on('response', function () {
+      console.log('Response Received! Downloading images...')
+    });
 }
 
 
 // The actual script!
 console.log("Welcome to the GitHub Avatar Downloader!");
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+const args = process.argv.slice(2);
+
+if (args.length !== 2) {
+  console.log("I need a user and a repo! Please provide these as two command line arguments!");
+  process.exit(0)
+}
+
+getRepoContributors(args[0], args[1], function (err, result) {
   if (err) throw err;
   const jsonResults = JSON.parse(result);
-  if (jsonResults.message === "Not Found"){
+  if (jsonResults.message === "Not Found") {
     console.log("No URLs Found!");
     return 0;
   }
-  for (user of jsonResults){
+  for (user of jsonResults) {
     downloadImageByURL(user.avatar_url, `./pics/${user.login}.jpg`)
   }
 })
