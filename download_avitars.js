f0a0aa9e8fdb7adcb4053ae8d40586ee2f7054f8
Lukas Horak <lukasbhorak@gmail.com>
2019-06-03 15:51:58 -0400

    finished assignment

--- download_avitars.js
+++ download_avitars.js
@@ -1,8 +1,17 @@
 const request = require('request');
 const secrets = require('./secrets');
+const fs = require('fs');
 
-//const downloadImageByURL(url, filePath)
+// Function to download avatars, given url
+const downloadImageByURL = (url, filePath) => {
+  request.get(url)
+  .on('error', function(err){
+    throw err;
+  })
+  .pipe(fs.createWriteStream(filePath))
+}
 
+// Get all contributors to repo, peform callback on them
 const getRepoContributors = (repoOwner, repoName, cb) => {
   const options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -17,19 +26,18 @@ const getRepoContributors = (repoOwner, repoName, cb) => {
   });
 }
 
+
+// The actual script!
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+  if (err) throw err;
   const jsonResults = JSON.parse(result);
-  let resultURLs = []
-
   if (jsonResults.message === "Not Found"){
     console.log("No URLs Found!");
-    return resultURLs
+    return 0;
   }
-
   for (user of jsonResults){
-    resultURLs.push(user.avatar_url)
+    downloadImageByURL(user.avatar_url, `./pics/${user.login}.jpg`)
   }
-  console.log(resultURLs);
 })
